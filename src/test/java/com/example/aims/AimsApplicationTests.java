4f8216d9e4d5605ce632508c59c738c2b2cd3117
Sonya-Jay <sonya.jayathilake@gmail.com>
2025-05-22 09:47:21 -0500

    Initial commit

---
+++ src/test/java/com/example/aims/AimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
